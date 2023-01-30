806b9be2e46c1a0b020de510aa52d0096306676e
dhavalvaja <dhavalgvaja.4188@gmail.com>
2023-01-30 11:24:44 +0530

    created pokemon form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'sample-angular';
+  pokemonForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.pokemonForm = fb.group({})
+  }
+
+  ngOnInit(): void {
+    this.pokemonForm = this.fb.group({
+      name: this.fb.control(''),
+      speciality: this.fb.control(''),
+      imgUrl: this.fb.control(''),
+    })
+  }
 }
